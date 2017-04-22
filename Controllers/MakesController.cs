5dbbbaa8791dc0058f9ae542e88288f9fd88752d
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-22 11:42:32 +0100

    Automapper, API Resources

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -2,14 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Controllers.Resources;
+using Vega.Models;
 
 namespace Vega.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Makes")]
+    //[Produces("application/json")]
+    //[Route("api/makes")]
     public class MakesController : Controller
     {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MakesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await _context.Makes.Include(m => m.Models).ToListAsync();
+            return Mapper.Map<List<Makes>, List<MakeResource>>(makes);
+        }
     }
 }
---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Vega.Controllers.Resources;
+using Vega.Models;
+
+namespace Vega.Mapping
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Makes, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
