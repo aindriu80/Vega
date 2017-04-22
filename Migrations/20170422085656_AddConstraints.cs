59027938690f0f0e9394cb1de693467fbfcd92ad
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-22 10:17:13 +0100

    Migrations - seed database

---
+++ Migrations/20170422085656_AddConstraints.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class AddConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Model",
+                newName: "Models");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Models",
+                newName: "Model");
+
+        }
+    }
+}
---
+++ Migrations/20170422090054_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert INTO Make (Name) VALUES('Make1')");
+            migrationBuilder.Sql("Insert INTO Make (Name) VALUES('Make2')");
+            migrationBuilder.Sql("Insert INTO Make (Name) VALUES('Make3')");
+
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelA',1)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelB',1)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelC',1)");
+
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelA',2)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelB',2)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelC',2)");
+
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelA',3)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelB',3)");
+            migrationBuilder.Sql("Insert INTO Models (Name, MakeID) VALUES('Make1-ModelC',3)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes");
+        }
+    }
+}
