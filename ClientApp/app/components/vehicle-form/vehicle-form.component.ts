35bb0fef0bf96c24417c0bfa453b60043dfbf506
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-27 16:59:23 +0100

    Implements cascading drop down linked to Make

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -8,15 +8,18 @@ import { MakeService } from "../../services/make.service";
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-makes;
+    makes:any[];
+    models:any[];
+    vehicle:any = {};
   constructor(private makeService: MakeService) { }
 
   ngOnInit() {
-    this.makeService.getMakes().subscribe(makes=>{
-    this.makes = makes;
-
-    console.log("Makes",this.makes);
-    });
+    this.makeService.getMakes().subscribe(makes=>
+    this.makes = makes);    
+    
   }
-
+    onMakeChange() {
+        var selectedMake = this.makes.find(m=> m.id == this.vehicle.make);
+        this.models= selectedMake ? selectedMake.models:[];
+    }
 }
