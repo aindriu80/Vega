39f2f290aa45b4b027bb5f0149a1a439c955e5f4
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-29 15:50:36 +0100

    Mapping API resources to Domain classes

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -10,15 +10,26 @@ using Vega.Models;
 
 namespace Vega.Controllers
 {
+    // place end point at top of class - all methods
+    // respond to api/vehicles
+
 
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
+        private readonly IMapper _mapper;
 
-        [HttpPost]
-
-        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        public VehiclesController(IMapper mapper)
         {
+            _mapper = mapper;
+        }
+        [HttpPost]
+        // not using domain classes - implementation details
+        // dealing with contracts, 
+
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = Mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             return Ok(vehicle);
         }
     }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -16,7 +16,13 @@ namespace Vega.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
-            CreateMap<Vehicle, VehicleResource>();
+
+            // API Resource to domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature {FeatureId = id})));
         }
     }
 }
