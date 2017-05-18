e4cc979d7084405d24f5c520ae9c1185ec707a0d
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-18 11:52:20 +0100

    File Validation

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Vega.Controllers.Resources;
 using Vega.Core;
@@ -16,6 +17,8 @@ namespace Vega.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
+        private readonly int MAX_BYTES = 1 * 1024 * 1024;
+        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
         private readonly IHostingEnvironment _host;
         private readonly IVehicleRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
@@ -34,6 +37,12 @@ namespace Vega.Controllers
             var vehicle = await _repository.GetVehicle(vehicleId, includeRelated: false);
             if (vehicle == null)
                 return NotFound();
+
+            if (file == null) return BadRequest("Null file");
+            if (file.Length == 0) return BadRequest("Empty file");
+            if (file.Length > MAX_BYTES) return BadRequest("Maximum file size reached");
+            if (ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type.");
+
 
             var uploadsFolderPath = Path.Combine(_host.WebRootPath, "uploads");
             // c:\wwwroot\
