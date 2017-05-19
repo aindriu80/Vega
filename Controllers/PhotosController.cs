c984f523ce82a3c4ee4a5a4c8fd5d4ae8cee74ee
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-19 10:09:41 +0100

    Recfactored to use json config file

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -2,9 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Vega.Controllers.Resources;
 using Vega.Core;
@@ -17,15 +17,17 @@ namespace Vega.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
-        private readonly int MAX_BYTES = 1 * 1024 * 1024;
-        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
+
+
         private readonly IHostingEnvironment _host;
         private readonly IVehicleRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly PhotoSettings _photoSettings;
 
-        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper, IOptionsSnapshot<PhotoSettings> options)
         {
+            _photoSettings = options.Value;
             _host = host;
             _repository = repository;
             _unitOfWork = unitOfWork;
@@ -40,9 +42,9 @@ namespace Vega.Controllers
 
             if (file == null) return BadRequest("Null file");
             if (file.Length == 0) return BadRequest("Empty file");
-            if (file.Length > MAX_BYTES) return BadRequest("Maximum file size reached");
-            if (ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type.");
+            if (file.Length > _photoSettings.MaxBytes) return BadRequest("Maximum file size reached");
 
+            if (!_photoSettings.IsSupported(file.FileName)) return BadRequest("Invalid file type.");
 
             var uploadsFolderPath = Path.Combine(_host.WebRootPath, "uploads");
             // c:\wwwroot\
