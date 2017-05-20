c0da7d4fb27906ebc182a2547c963bc5f05c4096
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-20 11:13:05 +0100

    Adding a bootstrap progress bar to uploading image

--- ClientApp/app/components/view-vehicle/view-vehicle.ts
+++ ClientApp/app/components/view-vehicle/view-vehicle.ts
@@ -1,7 +1,7 @@
 import { ProgressService } from './../../services/progress.service';
 import { ToastyService } from 'ng2-toasty';
 import { VehicleService } from './../../services/vehicle.service';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../../services/photo.service';
 
@@ -13,8 +13,10 @@ export class ViewVehicleComponent implements OnInit {
   vehicle: any;
   vehicleId: number; 
   photos: any[];
+  progress:any;
 
   constructor(
+    private zone: NgZone,
     private route: ActivatedRoute, 
     private router: Router,
     private toasty: ToastyService,
@@ -61,7 +63,14 @@ export class ViewVehicleComponent implements OnInit {
   var nativeElement: HTMLInputElement = this.fileInput.nativeElement;
 
   this.progressService.uploadProgress
-  .subscribe(progress => console.log(progress));
+  .subscribe(progress => {
+    console.log(progress);
+    this.zone.run(() => {
+       this.progress = progress;
+    });   
+  },
+  null,
+  ()=> { this.progress = null; });
   
   this.photoService.upload(this.vehicleId, nativeElement.files[0])
   .subscribe(photo => {
